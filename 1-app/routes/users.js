88755ba6ed4289c87367da9661e19e0016bd8541
yimiao <345230916@qq.com>
2017-11-21 12:42:49 +0800

    all the change

--- 1-app/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('what are u talking about');
-});
-
-module.exports = router;
