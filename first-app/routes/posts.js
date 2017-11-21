8e8649544484cd59cb81035d04dda22cc543e118
yimiao <345230916@qq.com>
2017-11-21 23:02:34 +0800

    js2_10

--- first-app/routes/posts.js
+++ first-app/routes/posts.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('posts', { title: 'postsa' });
+/* GET posts page. */
+router.get('/list', function(req, res, next) {
+  res.json({postsList: ['文章1', '文章2', '文章3'] });
 });
 
 module.exports = router;
